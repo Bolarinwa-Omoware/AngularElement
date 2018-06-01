7a445619f567c2c03793098734a13769e089cb09
Bolarinwa o. oluwaseun <bolarinwa@internal.interspacial.com>
2018-06-01 19:20:29 +0100

    angular element example one

---
+++ elements-build.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+
+(async function build() {
+    const files = [
+        './dist/angularElement/runtime.js',
+        './dist/angularElement/polyfills.js',
+        './dist/angularElement/scripts.js',
+        './dist/angularElement/main.js'
+    ]
+
+    await fs.ensureDir('elements')
+    await concat(files, 'elements/framework-poll.js')
+    await fs.copyFile('./dist/angularElement/styles.css', 'elements/styles.css')
+    await fs.copy('./dist/angularElement/assets/', 'elements/assets/')
+})()
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+import { FrameworkPollComponent } from './framework-poll/framework-poll.component';
+
+
+const config = {
+  apiKey: "AIzaSyCVLU4Frm3huhOGCUdXCbxGMrxmpM_8MQw",
+  authDomain: "framework-voter-db49a.firebaseapp.com",
+  databaseURL: "https://framework-voter-db49a.firebaseio.com",
+  projectId: "framework-voter-db49a",
+  storageBucket: "framework-voter-db49a.appspot.com",
+  messagingSenderId: "780462874260"
+}
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrameworkPollComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [
+    FrameworkPollComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor(private injector: Injector) {
+
+  }
+
+  ngDoBootstrap(){
+    const el = createCustomElement(FrameworkPollComponent, {injector: this.injector});
+    customElements.define('framework-poll', el);
+  }
+ }
